65f64cb53fb2484f14cc332d6a9c8502e3ca34b8
Anmol <rajpootanmol779@gmail.com>
2025-12-03 12:47:21 +0530

    upto handcontrolvolume

--- fingercounter.py
+++ fingercounter.py
@@ -1,1 +1,12 @@
-git remote add origin https://github.com/AnmolRajpoot25/Computervision_advanced.git
+import cv2
+import numpy as np
+import handTracingModue as htm
+
+cap = cv2.VideoCapture()
+while True:
+    success , img = cap.read()
+    if not success :
+        print("LOL")
+        break
+cv2.imshow('image', img)
+cv2.waitKey(1)
